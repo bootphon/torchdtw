a110b46938b6bfd727136f0b5c3568cbbe39191a
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-11-20 10:40:18 +0100

    ok

--- setup.py
+++ setup.py
@@ -6,19 +6,16 @@ import sys
 from setuptools import Extension, setup
 from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CppExtension, CUDAExtension
 
-TORCH_CUDA_ARCH_LIST = "Volta;Turing;Ampere;Ada;Hopper;Blackwell"
-TORCH_TARGET_VERSION = 0x020A000000000000
-
 
 def get_flags() -> tuple[list[str], list[str]]:
     """Return the compiler and linker flags."""
     match sys.platform:
         case "linux":
-            return ["-fdiagnostics-color=always", "-O3", "-fopenmp"], ["-fopenmp"]
+            return ["-Werror", "-fdiagnostics-color=always", "-O3", "-fopenmp"], ["-fopenmp"]
         case "win32":
-            return ["/O2", "/openmp"], []
+            return ["/WX", "/O2", "/openmp"], []
         case "darwin":  # On MacOS, we use the OpenMP version vendored by PyTorch
-            return ["-fdiagnostics-color=always", "-O3"], []
+            return ["-Werror", "-fdiagnostics-color=always", "-O3"], []
     raise RuntimeError(sys.platform)
 
 
@@ -27,10 +24,11 @@ def get_extension() -> Extension:
     use_cuda = CUDA_HOME is not None
     extension = CUDAExtension if use_cuda else CppExtension
     compiler_flags, linker_flags = get_flags()
-    extra_compile_args = {"cxx": [f"-DTORCH_TARGET_VERSION={TORCH_TARGET_VERSION}", *compiler_flags], "nvcc": ["-O3"]}
+    extra_compile_args = {"cxx": ["-DTORCH_TARGET_VERSION=0x020A000000000000", *compiler_flags], "nvcc": ["-O3"]}
     sources = ["src/torchdtw/csrc/dtw.cpp"]
     if use_cuda:
-        os.environ["TORCH_CUDA_ARCH_LIST"] = TORCH_CUDA_ARCH_LIST
+        if "TORCH_CUDA_ARCH_LIST" not in os.environ:
+            os.environ["TORCH_CUDA_ARCH_LIST"] = "Volta;Turing;Ampere;Ada;Hopper"
         sources.append("src/torchdtw/csrc/cuda/dtw.cu")
     return extension(
         "torchdtw._C",
